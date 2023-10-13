03827ffb3993b5874169fcaca75e674b69babea0
Pedro Corado <pedrocorado2025@gmail.com>
2023-10-13 01:18:46 -0300

    Ajuda Ebac

--- src/main.js
+++ src/main.js
@@ -3,16 +3,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const questions = document.querySelectorAll('[data-faq-question]');
     
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function (event) {
-            const abaAlvo = event.target.dataset.tabButton;
+        buttons[i].addEventListener('click', function (botao) {
+            const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
-            event.target.classList.add('shows__tabs__button--is-active');
+            buttons[i].classList.add('shows__tabs__button--is-active');
         })
     }
-    
+
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }

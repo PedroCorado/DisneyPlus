608b52880ce6f0076c19c8458ec5e41895aea380
Pedro Corado <pedrocorado2025@gmail.com>
2023-10-15 14:58:35 -0300

    Ajuda Ebac

--- src/main.js
+++ src/main.js
@@ -1,53 +1,72 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
-    
+
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = window.scrollY
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function (botao) {
-            const abaAlvo = botao.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+        buttons[i].addEventListener('click', function (event) {
+            const abaAlvo = event.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
             buttons[i].classList.add('shows__tabs__button--is-active');
-        })
+        });
     }
 
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
-    
-    // Adicionando este eventListener para o conteudo das respostas
-    const answers = document.querySelectorAll('.faq__questions__item__answer'); // Add this line
-    
+
+    const answers = document.querySelectorAll('.faq__questions__item__answer');
+
     for (let i = 0; i < answers.length; i++) {
-        answers[i].addEventListener('click', abreOuFechaResposta); 
+        answers[i].addEventListener('click', abreOuFechaResposta);
     }
-    //Fim do novo codigo das respostas
+
 })
 
-function abreOuFechaResposta(elemento) {
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header')
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header')
+    header.classList.remove('header--is-hidden')
+}
+
+function abreOuFechaResposta(event) {
     const classe = 'faq__questions__item--is-open';
-    const elementoPai = elemento.target.parentNode;
+    const elementoPai = event.target.parentNode;
 
     elementoPai.classList.toggle(classe);
 }
 
-
-
-function removeBotaoAtivo(){
+function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
-    for (let i = 0; i < buttons.length; i++ ) {
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
-
 }
 
-function escondeTodasAbas(){
+function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
-    for (let i = 0; i < tabsContainer.length; i++ ){
+    for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
 }
